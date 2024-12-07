29cff5ca10f0b7cddd660cc9dc5bf80b21356625
fallingflow <y30660912@gmail.com>
2024-12-07 23:37:03 +0900

    ShoppingNogi.sql 수정

--- ShoppingNogi.sql
+++ ShoppingNogi.sql
@@ -1,43 +1,40 @@
-SELECT * FROM ShoppingNogi.item;DROP DATABASE IF EXISTS `ShoppingNogi`;
-CREATE database `ShoppingNogi`;
-USE `ShoppingNogi`;
+    DROP DATABASE IF EXISTS `ShoppingNogi`;
+    CREATE DATABASE `ShoppingNogi`;
+    USE `ShoppingNogi`;
 
-CREATE TABLE `category`(
-  `category_id` INT NOT NULL AUTO_INCREMENT,
-  `category_name` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`category_id`)
-  );
+    CREATE TABLE `category`(
+        `category_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `category_name` VARCHAR(20) NOT NULL
+    );
 
-CREATE TABLE `item`(
-  `item_id` INT NOT NULL AUTO_INCREMENT,
-  `category_id` INT NOT NULL,
-  `item_name` VARCHAR(200) NOT NULL,
-  `item_display_name` VARCHAR(200) NOT NULL,
-  PRIMARY KEY (`item_id`),
-  FOREIGN KEY (`category_id`) references `category`
-  );
+    CREATE TABLE `item`(
+        `item_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `category_id` INT NOT NULL,
+        `item_name` VARCHAR(200) NOT NULL,
+        `item_display_name` VARCHAR(200) NOT NULL,
+        FOREIGN KEY (`category_id`) REFERENCES `category`(`category_id`)
+    );
 
-CREATE TABLE `item_option`(
-  `item_id` INT NOT NULL,
-  `option_type` VARCHAR(40),
-  `option_sub_type` VARCHAR(40),
-  `option_value` VARCHAR(40),
-  `option_value2` VARCHAR(40),
-  `option_desc` VARCHAR(500),
-  FOREIGN KEY (`item_id`) references `item`
-  );
+    CREATE TABLE `item_option`(
+        `item_id` INT NOT NULL,
+        `option_type` VARCHAR(40),
+        `option_sub_type` VARCHAR(40),
+        `option_value` VARCHAR(40),
+        `option_value2` VARCHAR(40),
+        `option_desc` VARCHAR(500),
+        FOREIGN KEY (`item_id`) REFERENCES `item`(`item_id`)
+    );
 
-CREATE TABLE `auction_history`(
-  `auction_buy_id` INT NOT NULL,
-  `date_auction_buy` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`auction_buy_id`)
-  );
+    CREATE TABLE `auction_history`(
+        `auction_buy_id` INT NOT NULL PRIMARY KEY,
+        `date_auction_buy` VARCHAR(30) NOT NULL
+    );
 
-CREATE TABLE `auction_history_desc`(
-  `item_id` INT NOT NULL,
-  `auction_buy_id` INT NOT NULL,
-  `auction_price_per_unit` INT NOT NULL,
-  `item_count` INT NOT NULL,
-  FOREIGN KEY (`item_id`) references `item`,
-  FOREIGN KEY (`auction_buy_id`) references `auction_history`  
-  );
+    CREATE TABLE `auction_history_desc`(
+        `item_id` INT NOT NULL,
+        `auction_buy_id` INT NOT NULL,
+        `auction_price_per_unit` INT NOT NULL,
+        `item_count` INT NOT NULL,
+        FOREIGN KEY (`item_id`) REFERENCES `item`(`item_id`),
+        FOREIGN KEY (`auction_buy_id`) REFERENCES `auction_history`(`auction_buy_id`)
+    );
